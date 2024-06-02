6a65d2150efc080fad23cb507bb82a1b53925688
GopalSutar5683 <134592193+GopalSutar5683@users.noreply.github.com>
2024-06-03 02:48:16 +0530

    Create TodoAdapter.java

---
+++ http:/http:/http:/TodoAdapter.java
@@ -0,0 +1,46 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
+
+    private List<TodoItem> todoList;
+
+    public TodoAdapter(List<TodoItem> todoList) {
+        this.todoList = todoList;
+    }
+
+    @NonNull
+    @Override
+    public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_todo, parent, false);
+        return new TodoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TodoViewHolder holder, int position) {
+        TodoItem todoItem = todoList.get(position);
+        holder.textViewTitle.setText(todoItem.getTitle());
+        holder.textViewDescription.setText(todoItem.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return todoList.size();
+    }
+
+    static class TodoViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewTitle;
+        TextView textViewDescription;
+
+        TodoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+        }
+    }
+}
