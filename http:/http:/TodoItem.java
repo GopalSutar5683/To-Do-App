14bab481b89ade21e2d0f2ebf6cfc2394b6a249a
GopalSutar5683 <134592193+GopalSutar5683@users.noreply.github.com>
2024-06-03 02:47:26 +0530

    Create TodoItem.java

---
+++ http:/http:/TodoItem.java
@@ -0,0 +1,17 @@
+public class TodoItem {
+    private String title;
+    private String description;
+
+    public TodoItem(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
